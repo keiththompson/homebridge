7b9e0fe0bdfeb9a9d53d0f78e6fb86abd8e0906a
Keith Thompson <keithjoethompson@gmail.com>
2023-12-23 14:38:29 +0000

    initial commit

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/keiththompson/homebridge-client-go
+
+go 1.21.5
+
+require github.com/go-resty/resty/v2 v2.10.0
+
+require golang.org/x/net v0.17.0 // indirect
---
+++ main.go
@@ -0,0 +1,69 @@
+package homebridge
+
+import (
+    "github.com/go-resty/resty/v2"
+    "log"
+	"encoding/json"
+)
+
+type APIClient struct {
+    httpClient *resty.Client
+    BaseURL    string
+    Token      string
+}
+
+// NewAPIClient creates a new API client instance
+func NewAPIClient(baseURL string) *APIClient {
+    return &APIClient{
+        httpClient: resty.New(),
+        BaseURL:    baseURL,
+    }
+}
+
+type LoginCredentials struct {
+    Username string `json:"username"`
+    Password string `json:"password"`
+    OTP      string `json:"otp,omitempty"`
+}
+
+type LoginResponse struct {
+    Token string `json:"token"`
+}
+
+func (c *APIClient) Login(username, password, otp string) error {
+    credentials := LoginCredentials{
+        Username: username,
+        Password: password,
+        OTP:      otp,
+    }
+
+    resp, err := c.httpClient.R().
+        SetHeader("Content-Type", "application/json").
+        SetBody(credentials).
+        Post(c.BaseURL + "/api/auth/login")
+
+    if err != nil {
+        return err
+    }
+
+    var loginResp LoginResponse
+    if err := json.Unmarshal(resp.Body(), &loginResp); err != nil {
+        return err
+    }
+
+    c.Token = loginResp.Token
+    return nil
+}
+
+func (c *APIClient) getServerPairing(endpoint string) {
+    // Example: GET request using the token
+    resp, err := c.httpClient.R().
+        SetAuthToken(c.Token).
+        Get(c.BaseURL + endpoint)
+
+    if err != nil {
+        log.Fatalf("Error on request: %v", err)
+    }
+
+    log.Printf("Response: %v", resp)
+}
